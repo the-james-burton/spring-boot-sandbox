06c2258ff0fc350cc69e19ca762c19c894eb24dc
the-james-burton <the.james.burton@gmail.com>
2018-06-25 19:44:23 +0100

    initial commit from start.spring.io

---
+++ src/main/java/com/jimsey/springbootsandbox/SpringBootSandboxApplication.java
@@ -0,0 +1,12 @@
+package com.jimsey.springbootsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSandboxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jimsey/springbootsandbox/SpringBootSandboxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jimsey.springbootsandbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSandboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
